42f7cb796edcbc28eb2a32d519abbd7ee9cccb64
JC <chohy1106@gmail.com>
2022-10-15 15:19:20 +0900

    yester

--- 20221011_D11/accounts/urls.py
+++ 20221011_D11/accounts/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "accounts"
-urlpatterns = []
+urlpatterns = [
+    path('signup/', views.signup, name="signup"),
+    path('<int:pk>/', views.detail, name="detail"),
+]
--- 20221012_D12/accounts/views.py
+++ 20221012_D12/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
-from .models import User
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def signup(request):
@@ -10,18 +12,29 @@ def signup(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('movie_crud:index')
+            return redirect("movie_crud:index")
     else:
         form = CustomUserCreationForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/signup.html', context)
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
 
 def detail(request, pk):
 
     user = get_user_model().objects.get(pk=pk)
+    context = {"user": user}
+    return render(request, "accounts/detail.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("movie_crud:index")
+    else:
+        form = AuthenticationForm()
     context = {
-        'user': user
+        "form": form,
     }
-    return render(request, 'accounts/detail.html', context)
+    return render(request, "accounts/login.html", context)
