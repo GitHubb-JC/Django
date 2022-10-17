6e9cbe1522753d08095a7e34c1da4b6a5504f9bf
JC <chohy1106@gmail.com>
2022-10-17 17:28:47 +0900

    1017

---
+++ 20221017_D/reviews/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=80)
+    grade = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 80},
+    )
