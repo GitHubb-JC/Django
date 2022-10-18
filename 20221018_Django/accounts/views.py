d0b47c7090f8d04a24acfc56b94b197b22ca06cf
JC <chohy1106@gmail.com>
2022-10-18 17:45:07 +0900

    account mypage, update, navbar ing

--- 20221018_Django/accounts/views.py
+++ 20221018_Django/accounts/views.py
@@ -1,9 +1,11 @@
+from difflib import context_diff
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -45,3 +47,30 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect("accounts:index")
+
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
+
+
+@login_required
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:mypage")
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/update.html", context)
+
+
+def mypage(request):
+    return render(request, "accounts/mypage.html")
