ee812beccbde36c5a5e5ffe5a58dac6496412a8d
JC <chohy1106@gmail.com>
2022-10-25 17:08:25 +0900

    like model

---
+++ 20221024_D/reviews/models.py
@@ -0,0 +1,32 @@
+from distutils.text_file import TextFile
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.conf import settings
+
+# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=80)
+    grade_Choices = (
+        ("⭐", "⭐"),
+        ("⭐⭐", "⭐⭐"),
+        ("⭐⭐⭐", "⭐⭐⭐"),
+        ("⭐⭐⭐⭐", "⭐⭐⭐⭐"),
+        ("⭐⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"),
+    )
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_articles"
+    )
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
